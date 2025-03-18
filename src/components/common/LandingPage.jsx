ae9b3951f1c3e38b7e6660eb29c9681bbe24b665
Prapti Shah <praptishah004@gmail.com>
2025-03-18 23:46:48 +0530

    changes

--- src/components/common/LandingPage.jsx
+++ src/components/common/LandingPage.jsx
@@ -33,7 +33,7 @@ const LandingPage = () => {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
-                  <Link className="nav-link" to="/explore">
+                  <Link className="nav-link" to="/exploreitems">
                     Explore Items
                   </Link>
                 </li>
